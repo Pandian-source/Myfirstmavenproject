1c462ff2182822aee6e71c14e91c29bede3abc0f
Pandian-source <srilanakamrst@gmail.com>
2020-12-25 17:10:10 +0530

    Login issue fixed

--- src/test/java/Firstpackage/Firstclass.java
+++ src/test/java/Firstpackage/Firstclass.java
@@ -3,7 +3,6 @@ package Firstpackage;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -43,13 +42,14 @@ public void gettitle() throws IOException
      	  testCase.log(Status.INFO,"Successfully Got the title");
 		  System.out.println("Window maximised");
 		  String baseUrl = "https://b2bui2.tltid.com/login";
-		  String expectedTitle = "ClaritySSO - Login";
+		  String expectedTitle = "Login";
 	      String actualTitle = "";
 	      driver.get(baseUrl);
           actualTitle = driver.getTitle();
+          System.out.println(actualTitle);
 	      if (actualTitle.contentEquals(expectedTitle))
 	      {
-	           //System.out.println("Test Passed!");
+	         
 	           testCase.log(Status.PASS," Test Passed");
 	        } 
 	      else 
@@ -62,12 +62,13 @@ public void loginbutton() throws InterruptedException
 {	 
 	  System.out.println("driver="+driver);
  	  testCase = extentReport.createTest("Loginbutton");
-      WebDriverWait wait =new WebDriverWait(driver,10);
-      WebElement Element= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//a[1][@class=\"hero-login-link btn btn-primary\"]")));
+      WebDriverWait wait =new WebDriverWait(driver,100);
+      Thread.sleep(10);
+      WebElement Element= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']")));
 	  String b = Element.getText();
 	  System.out.println(b);
       Element.click();
-      if(b.contentEquals("LOGIN"))
+      if(b.contentEquals("Login"))
       {
     	  testCase.log(Status.PASS,"Text Equal");
       }
@@ -75,57 +76,49 @@ public void loginbutton() throws InterruptedException
       {
     	  testCase.log(Status.FAIL,"Text Not Equal");
       }
-      JavascriptExecutor js1 = (JavascriptExecutor) driver;		
-      js1.executeScript("alert('Welcome to B2B PANDIAN');");
-      String text = driver.switchTo().alert().getText();			
-      System.out.println(text);
-      Alert alert = driver.switchTo().alert();
-      alert.accept();
+     
 	}
-@Test
-public void username() throws IOException
-  	{
-	  System.out.println("driver="+driver);
- 	  testCase = extentReport.createTest("Username Test");
- 	  JavascriptExecutor js = (JavascriptExecutor) driver;
-      js.executeScript("window.scrollBy(0,350)", "");
- 	  this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	  WebElement username = this.driver.findElement(By.xpath(".//input[@placeholder=\"Your Email *\"]"));
-      username.click();
-      System.out.println("username field clicked");
-      username.sendKeys("a.pandian@wintlt.com");
-      System.out.println("Username field entered");
-  	}
 @Test
 public void password() throws IOException
       {
 	  System.out.println("driver="+driver);
  	  testCase = extentReport.createTest("password and Login success");
       this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-      WebElement password = this.driver.findElement(By.xpath(".//input[@placeholder=\"Password *\"]"));
+      WebElement username = this.driver.findElement(By.xpath("//input[@type='text']"));
+      username.click();
+      System.out.println("username field clicked");
+      username.sendKeys("a.pandian@wintlt.com");
+      System.out.println("Username field entered");
+      WebElement password = this.driver.findElement(By.xpath("//input[@type='password']"));
       password.click();
       System.out.println("password field clicked");
       password.sendKeys("12345S#");
       System.out.println("password field entered");
-      WebElement button = driver.findElement(By.xpath(".//button[text()=\"Login\"]"));
+      WebElement button = driver.findElement(By.xpath("//button[@type='submit']"));
       JavascriptExecutor js = (JavascriptExecutor) driver;
       js.executeScript("arguments[0].click();", button);
     }
+
 @Test
-public void password1() throws IOException
+public void password1() throws IOException, InterruptedException
 {
       System.out.println("driver="+driver);
  	  testCase = extentReport.createTest("searchform");
-      this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-      WebElement password1 = this.driver.findElement(By.xpath(".//span[text()=\"ClaritySSO\"]/following::input[@placeholder=\"From?\"]"));
-      password1.click();
-      JavascriptExecutor js2 = (JavascriptExecutor) driver;		
-      js2.executeScript("alert('All actions performed PANDIAN');");
-      String action = this.driver.switchTo().alert().getText();			
-      System.out.println(action);
-      Alert alert1 = this.driver.switchTo().alert();
-      alert1.accept();
+      this.driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+      WebDriverWait wait2 = new WebDriverWait(driver, 100);
+      Thread.sleep(30000);
+      wait2.until(ExpectedConditions.elementToBeClickable(By.xpath("(//input[@type='text'])[2]"))).click();
       }
+
+@Test
+public void tofield() throws IOException
+{
+	  System.out.println("driver="+driver);
+	  testCase = extentReport.createTest("searchform");
+	  this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+      WebDriverWait wait2 = new WebDriverWait(driver, 10);
+      wait2.until(ExpectedConditions.elementToBeClickable(By.xpath("(//input[@type='text'])[3]"))).click();
+}
 @AfterSuite
 public void close()
 	      {   
